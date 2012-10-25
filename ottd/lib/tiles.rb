65b5234b00bb7a657e9171d9d0e1449161c95ae4
Ronald Hecker <selsery@gmail.com>
2012-10-25 14:59:37 +0200

    Add another building to make it pertier

--- ottd/lib/tiles.rb
+++ ottd/lib/tiles.rb
@@ -4,14 +4,17 @@ class Tiles
 
   def initialize
     # @tile = Tile.new
-    @building = Tile.new("building1.png", 1,2)
+    @buildings = [
+      Tile.new("building1.png", 1,2),
+      Tile.new("building2.png", 1,2)
+    ]
   end
 
 
   def draw
-    @building.draw(2,0)
-    @building.draw(1,0)
-    @building.draw
+    @buildings[0].draw(2,0)
+    @buildings[1].draw(1,0)
+    @buildings[0].draw
     # 5.times do |x|
     #   5.times do |y|
     #   end
